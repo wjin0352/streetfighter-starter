1938fa028b087394189265465fa8654f1c765c01
won <wjin0352@gmail.com>
2016-03-21 16:16:05 -0400

    added jquery to hadouken and ryu divs

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
-// $(document).ready(function(){
-//   $('.fighter-image').click(function(){
-//     $('#test').text('wtf');
-//   });
-// });
-
-
-// $(document).ready(function(){
-//  $('#test').click(animate({
-//             left: 300
-//   }, 'slow'));
-//   setTimeout(update, 1000);
-// });
-
+$(document).ready(function(){
+  $('.ryu').mouseenter(function(){
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function(){
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function(){
+    // play hadouken sound
+    // show hadouken and animate it to right of screen
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+  })
+  .mouseup(function(){
+    // ryu goes back to ready position
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+});
