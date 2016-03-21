8fc7f0f2d0db67fb68c32a958dfb318540ec8e15
won <wjin0352@gmail.com>
2016-03-21 15:22:03 -0400

    created divs for ryu, hadouken.  addes css for those divs backgroundimages

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//   $('.fighter-image').click(function(){
+//     $('#test').text('wtf');
+//   });
+// });
+
+
+// $(document).ready(function(){
+//  $('#test').click(animate({
+//             left: 300
+//   }, 'slow'));
+//   setTimeout(update, 1000);
+// });
+
