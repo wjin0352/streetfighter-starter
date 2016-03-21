c3a086c84296cbae1a17b0fa9bdc6a44108b8753
won <wjin0352@gmail.com>
2016-03-21 16:34:24 -0400

    added mp3 hadouken for hadouken action

--- js/app.js
+++ js/app.js
@@ -1,22 +1,36 @@
 $(document).ready(function(){
-  $('.ryu').mouseenter(function(){
+  $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
-  .mouseleave(function(){
+  .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
-  .mousedown(function(){
+  .mousedown(function() {
     // play hadouken sound
     // show hadouken and animate it to right of screen
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
+    $('.hadouken').finish().show().animate(
+      {'left':'1320px'},
+      800,
+        function() {
+          $(this).hide();
+          $(this).css('left','520px');
+        }
+    );
   })
   .mouseup(function(){
     // ryu goes back to ready position
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+
+  function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
 });
